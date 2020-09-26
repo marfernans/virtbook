db8fc7f093fff57133e5d97e9a9a19d86609ee7f
Mario Fernandez Serrano <me.fernandezserrano@gmail.com>
2020-09-25 20:44:49 -0500

    I did Configuration webpack.config, packge.json and basic template home

---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+
+const Home =  () =>  {
+
+    const view = ` 
+    <section class="container">
+
+    <div class="content__card">
+
+        <div class="content__card--texts">
+            <a href="#/1/">
+            <img src="image" alt="portada">
+            <h1 class="title">Title Book</h1>
+            <p class="author">Name author</p>
+        </div>
+    </div>
+
+    </section>
+
+    `;
+
+    return view
+
+};
+
+
+export default Home;
