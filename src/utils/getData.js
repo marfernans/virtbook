cd270c94a7e42e937c5454b6fb9393ce8d56c43b
Mario Fernandez Serrano <me.fernandezserrano@gmail.com>
2020-09-30 21:55:13 -0500

    add file getdata

---
+++ src/utils/getData.js
@@ -0,0 +1,16 @@
+const API = "http://openlibrary.org/people/george08/lists/OL97L/seeds.json";
+
+const getData = async (id) => {
+
+     const apiURL = id ?`${API}${id}`: API;
+    try {
+      const response = await fetch(apiURL);
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+    console.log('Fetch Error', error);
+    };
+};
+
+export default getData;
