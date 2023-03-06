759fb1ffae80670617475faf8730a5f57a636230
Mario Fernandez Serrano <mefernandez@unimagdalena.edu.co>
2023-03-06 10:40:38 -0500

    [ADDED] Views of current searches and css code to searches not found.

--- components/Router.js
+++ components/Router.js
@@ -2,41 +2,58 @@
 import apiendpoint from "../helpers/wp_api.js"
 import { reqajax } from "../helpers/req_ajax.js"
 import { PostCard } from "./PostCard.js"
-import { Post } from "./Post.js";
+import { Post } from "./Post.js"
+import { SearchCard } from "./SearchCard.js"
 
 
 
 export async function Router() {
     const d = document,
-        $main = d.getElementById("main");
-    //win = window;
+        $main = d.getElementById("main")
 
-    let { hash } = location;
-    console.log(hash);
+    let { hash } = location
+    console.log(hash)
 
-    $main.innerHTML = null;
+    $main.innerHTML = null
 
     if (!hash || hash === "#/") {
         await reqajax({
             url: apiendpoint.POSTS,
             cbSuccess: (posts) => {
-                console.log(posts);
-                let html = "";
-                posts.forEach((post) => html += PostCard(post));
-                d.querySelector(".loader").style.display = "none";
-                $main.innerHTML = html;
+                console.log(posts)
+                let html = ""
+                posts.forEach((post) => html += PostCard(post))
+                d.querySelector(".loader").style.display = "none"
+                $main.innerHTML = html
             }
         })
         console.log(apiendpoint.POST)
     } else if (hash.includes("#/search")) {
-        let query = localStorage.getItem("userSearch");
+        let query = localStorage.getItem("userSearch")
 
-        if (!query) return false;
-
+        if (!query) {
+            d.querySelector(".loader").style.display = "none"
+            return false;
+        }
         await reqajax({
             url: `${apiendpoint.SEARCH}${query}`,
             cbSuccess:(search) => {
-                console.log(search);
+                console.log(search)
+                let html = ""
+
+                if (search.length === 0){
+                    html = `
+                    <p class= "error">
+                     No existen resultados en tu busqueda para la palabra <mark>${query}</mark>
+                    </p>
+                     `;
+                } else {
+                    search.forEach(post => (html += SearchCard(post)))
+
+                }
+
+                $main.innerHTML = html
+
             }
         })
 
@@ -49,7 +66,7 @@ export async function Router() {
             url: `${apiendpoint.POST}/${localStorage.getItem("postId")}`,
             cbSuccess: (post) => {
                 console.log(post);
-                $main.innerHTML = Post(post);
+                $main.innerHTML = Post(post)
                 /*
 
                 d.querySelector(".loader").style.display = "none";
